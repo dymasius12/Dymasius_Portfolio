7b5c401164d97a03e9b839ab585b0f0915128438
dymasius12 <dymasius12@gmail.com>
2023-11-18 21:20:42 +0800

    done made on Sat Nov 18 21:20:42 +08 2023

--- app.py
+++ app.py
@@ -70,13 +70,35 @@ image = Image.open(image_path)
 # Display the image in the sidebar
 st.sidebar.image(image, caption='Dymasius Yusuf Sitepu')
 
+# Interest
+with st.sidebar:
+    st.header("INTERESTS")
+    st.write("My professional interests include financial technology, software engineering, web development, machine learning, business strategy & development, big data & cloud, and quantitative trading.")
+
+with st.sidebar:
+
+    # Languages & Skills
+    with st.expander("LANGUAGES"):
+        st.write("Indonesian Language (Native)")
+        st.write("English Language (Advanced)")
+        st.write("Japanese Language (Intermediate)")
+        st.write("Malay Language (Advanced)")
+        st.write("Mandarin Language (Beginner)")
+
+    # Achievements
+    with st.expander("ACHIEVEMENTS"):
+        st.write("GlobalFoundries Appreciation Award 2023")
+        st.write("GlobalFoundries Appreciation Award 2022")
+        st.write("GlobalFoundries Spotlight Award 2023")
+        st.write("GlobalFoundries Spotlight Award 2022")
+        st.write("2nd Place Hackathon 2020+1 University College London Code Club")
+        st.write("Top 15 out of 655 teams in Shopee Ultimate Case Competition 2021")
+        st.write("First runner-up in NUS Public Art Competition")
+        st.write("Finisher at Osim Sundown 21KM 2023 & SCSM 21KM 2022")
 # Sidebar Section
 with st.sidebar:
-    if st.button('LinkedIn'):
+    if st.button('LINKEDIN'):
         webbrowser.open("https://linkedin.com/in/dymasius/")
-
-    if st.button('GitHub'):
-        webbrowser.open("https://github.com/dymasius12")
 
 ###########END OF SIDEBAR
 
@@ -86,16 +108,15 @@ ntu_image = "img/ntu.png"
 nus_image = "img/nus.png"
 stanford_image = "img/stanford.png"
 
-st.header("🎓 Education")
+st.header("🎓 EDUCATION")
 
 # NTU Information
 col1, col2, col3 = st.columns([1, 4, 2])
 with col1:
     st.image(ntu_image, width=90)
 with col2:
-    st.subheader("NANYANG TECHNOLOGICAL UNIVERSITY (NTU), Singapore")
-    st.write("Master of Science (MSc)")
-    st.write("Financial Technology, Intelligent Process Automation")
+    st.subheader("Nanyang Technological University (NTU) or (南洋理工大学), Singapore")
+    st.write("Master of Science (MSc), Financial Technology, Intelligent Process Automation")
 with col3:
     st.write("Graduation Date: 2024")
 
@@ -104,9 +125,8 @@ col1, col2, col3 = st.columns([1, 4, 2])
 with col1:
     st.image(nus_image, width=90)
 with col2:
-    st.subheader("NATIONAL UNIVERSITY OF SINGAPORE (NUS), Singapore")
-    st.write("Bachelor of Engineering Science (BEng)")
-    st.write("Computational Engineering Science")
+    st.subheader("National University of Singapore (NUS) or (新加坡國立大學), Singapore ")
+    st.write("Bachelor of Engineering Science (BEng), Computational Engineering Science")
 with col3:
     st.write("Graduation Date: 2021")
 
@@ -115,7 +135,7 @@ col1, col2, col3 = st.columns([1, 4, 2])
 with col1:
     st.image(stanford_image, width=80)
 with col2:
-    st.subheader("STANFORD UNIVERSITY California, USA")
+    st.subheader("Stanford University, California, USA")
     st.write("International Honours Program (Summer Exchange)")
     st.write("Organizing for Good, Design Thinking")
 with col3:
@@ -125,7 +145,7 @@ with col3:
 
 # Create a container to organize content using Streamlit's container feature
 with st.container():
-    st.header('⚒️ Skills')
+    st.header('⚒️ SKILLS')
     col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
     with col1:
         st_lottie(python_lottie, height=70,width=70, key="python", speed=2.5)
@@ -144,60 +164,131 @@ with st.container():
     with col4:
         st_lottie(js_lottie,height=50,width=50, key="js", speed=1)
 
+st.header("🔬 RESEARCH EXPERIENCE")
+
+# Nanyang Technological University Experience
+col1, col2, col3 = st.columns([1, 5, 3])
+with col1:
+    st.image(ntu_image, width=90)
+with col2:
+    st.subheader("Nanyang Technological University. Singapore, SG")
+    st.write("Quantitative AI/ML Researcher, Part-Time")
+    st.markdown("""
+        - Under NTU School of Computer Science and Engineering. PI Prof Zhang Jie, and Co-PI Dr Li Fang.
+        - Quantitative and Machine Learning Researcher for funded project 2023-2025 MOE Tier-1.
+        - Utilized Python for Back-End creation of Artificial Intelligence for Quantitative Trading.
+    """)
+with col3:
+    st.write("Aug 2023 - Now")
+
+# Stanford University Experience
+col1, col2, col3 = st.columns([1, 5, 3])
+with col1:
+    st.image(stanford_image, width=80)
+with col2:
+    st.subheader("Stanford University. California, USA")
+    st.write("Research Engineer, Part-Time")
+    st.markdown("""
+        - Collaborated with Stanford University faculty to co-author a research paper.
+        - "Effective Approaches to Disaster Evacuation During a COVID-Like Pandemic".
+        - Developed a deterministic SEIR model simulation using R and Python.
+    """)
+with col3:
+    st.write("Jul 2021 - Feb 2022")
+
 # Work Experience Section
-st.header("🧑‍💼 Work Experience")
+st.header("🧑‍💼 WORK EXPERIENCE")
 st.subheader("GlobalFoundries Inc. Singapore")
 st.write("Database & TapeOut Automation Engineer, Full-Time. Mar 2022 - Present")
 st.markdown("""
-- Implemented advanced machine learning algorithms...
-- Streamlined database enablement processes...
+- Implemented advanced machine learning algorithms, including linear regression and XGBoost, to accurately forecast probe
+card tip length and extend its lifetime by 25%, resulting in cost savings of over 1000 SGD per month.
+- Streamlined database enablement processes through integration with Python Automation, reducing data processing time
+by 50% and improving overall efficiency.
+- Developed an internal Website and 24/7 support chatbot using Python, PHP, Bootstrap 5, HTML, CSS and JavaScript to
+streamline communication between teams and increase efficiency by providing Self-updating PowerBI Dashboard and
+Self-help MySQL Client.
 """)
-st.subheader("GlobalFoundries Inc. Singapore")
-st.write("Database & TapeOut Automation Engineer, Full-Time. Mar 2022 - Present")
+st.subheader("KOOPrime Pte Ltd. Singapore")
+st.write("Software Engineer, Intern. Jun 2021 - Jul 2021")
 st.markdown("""
-- Implemented advanced machine learning algorithms...
-- Streamlined database enablement processes...
+- Developed and implemented software solutions in C# .Net and Java Spring Framework to optimize laboratory processes,
+resulting in a 30% reduction in errors.
 """)
-st.subheader("GlobalFoundries Inc. Singapore")
-st.write("Database & TapeOut Automation Engineer, Full-Time. Mar 2022 - Present")
+st.subheader("Analog Devices, Inc. Singapore")
+st.write("Data Analyst and Business Intelligence, intern. Jan 2020 - Jul 2020")
 st.markdown("""
-- Implemented advanced machine learning algorithms...
-- Streamlined database enablement processes...
+- Created self-updating visualizations in Power BI to analyze survey responses and identify key areas for improvement,
+resulting in a 15% increase in overall customer satisfaction scores.
 """)
 
-# Repeat this pattern for other experiences
 
-# Research Experience Section
-st.header("Research Experience")
-# Add your research experience details here
 
-# Leadership Experience Section
-st.header("Leadership Experience")
-# Add your leadership experience details here
+# Project details displayed in main area based on selection
+st.header("📁 PROJECT EXPERIENCE")
+# Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
+    st.session_state.visibility = "hidden"
+    st.session_state.disabled = False
+#Dropdown for selecting a project
 
-# Volunteer Experience Section
-st.header("Volunteer Experience")
-# Add your volunteer experience details here
+selected_project = st.selectbox(
+    "Select a Project",
+    ["Credit-Card-Fraud-Detection", "The Motley S.M.A.R.T", "News_API", "UCL-IHI-Hackathon2021"],
+    label_visibility=st.session_state.visibility,
+)
 
-# Project Experience Section
-st.header("Project Experience")
-# Add your project experience details here
+if selected_project == "The Motley S.M.A.R.T":
+    with st.expander("The Motley S.M.A.R.T", expanded=True):
+        st.write("""
+        The Motley S.M.A.R.T is a desktop application that provides insights and visualization for stocks, 
+        specifically the S&P 500 companies. With an intuitive interface, users can either select a company 
+        from a dropdown list or input a stock code directly to view comprehensive stock details or visualize 
+        stock price trends.
+        """)
+        st.markdown("[GitHub Page](https://github.com/dymasius12/TheMotleySMART)")
+        st.markdown("""
+        - **Company Selection**: Choose a company from the S&P 500 list via a dropdown.
+        - **Stock Dashboard**: Get a detailed dashboard view of the selected stock.
+        - **Stock Price vs. Time Graph**: Visualize the stock's closing prices over time.
+        """)
 
-# Languages & Skills Section
-st.header("Languages & Skills")
-# Add your languages and skills details here
+elif selected_project == "News_API":
+    with st.expander("News_API", expanded=True):
+        st.write("""
+        News_API is a python script that tries to get the latest news.
+        """)
+        st.markdown("[GitHub Page](https://github.com/dymasius12/News_API)")
+        st.write("""
+        - This script allows you to fetch the latest news for a given stock company name using the NewsAPI.
+        """)
 
-# Achievements Section
-st.header("Achievements")
-# Add your achievements details here
+elif selected_project == "UCL-IHI-Hackathon2021":
+    with st.expander("UCL-IHI-Hackathon2021", expanded=True):
+        st.write("""
+        The IHI Code Club Hackathon 2020+1 is a full-day collaborative hacking sprint to solve real-world 
+        issues in health data science. As a participant, you had the opportunity to learn from and collaborate 
+        with other participants from diverse backgrounds.
+        """)
+        st.markdown("[GitHub Page](https://github.com/dymasius12/UCL-IHI-Hackathon2021)")
+        st.write("""
+        - Task: Tackling a public health problem related to air quality and respiratory illness in the US.
+        """)
 
-# Interests Section
-st.header("Interests")
-st.write("My professional interests include financial technology, software engineering, web development, machine learning, business strategy & development, big data & cloud, and quantitative trading.")
+elif selected_project == "Credit-Card-Fraud-Detection":
+    with st.expander("Credit-Card-Fraud-Detection", expanded=True):
+        st.write("""
+        It is important that credit card companies are able to recognize fraudulent credit card transactions so that customers are not charged for items that they did not purchase. This app is to help identify the fraudulent credit card transactions.
+        """)
+        st.markdown("[Web App](https://ntu-msft-mh6804.streamlit.app/)")
+        st.write("""
+        - Task: Tackling Credit Card Fraud Detection.
+        """)
+
 
 
 # -----------------  contact  ----------------- #
-st.subheader("📨 Contact Me")
+st.subheader("📨 CONTACT ME")
 contact_form = f"""
 <form action="https://formsubmit.co/{info["Email"]}" method="POST">
     <input type="hidden" name="_captcha value="false">
